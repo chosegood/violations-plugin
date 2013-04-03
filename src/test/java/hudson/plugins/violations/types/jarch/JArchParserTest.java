b4c7f1068dc9219d2399219508d82651d60b9c5f
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2013-04-03 15:57:15 +0100

    Added testParseWithMultipleRuleset

--- src/test/java/hudson/plugins/violations/types/jarch/JArchParserTest.java
+++ src/test/java/hudson/plugins/violations/types/jarch/JArchParserTest.java
@@ -10,10 +10,13 @@ import hudson.plugins.violations.ViolationsParserTest;
 import hudson.plugins.violations.model.FullBuildModel;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 
 public class JArchParserTest extends ViolationsParserTest {
+
+    static final Logger logger = Logger.getLogger(JArchParserTest.class.toString());
 
     protected FullBuildModel getFullBuildModel(String filename) throws IOException {
         ViolationsParser parser = new JArchParser();
@@ -21,16 +24,21 @@ public class JArchParserTest extends ViolationsParserTest {
     }
 
     @Test
-    public void testParseWithSingleFile() throws Exception {
+    public void testParseWithSingleRuleset() throws Exception {
         FullBuildModel model = getFullBuildModel("oneruleset.xml");
-
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(model);
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
         // check number of violations and number of files
         assertEquals(2, model.getCountNumber(JArchParser.TYPE_NAME));
         assertEquals(1, model.getFileModelMap().size());
+    }
+
+    @Test
+    public void testParseWithMultipleRuleset() throws Exception {
+        FullBuildModel model = getFullBuildModel("jarch.xml");
+
+        // check number of violations and number of files
+        assertEquals(4, model.getCountNumber(JArchParser.TYPE_NAME));
+        assertEquals(2, model.getFileModelMap().size());
     }
 
 }
